136b38ba251aa2e0956156194190574e28ef43bb
ポテト <fries49923@gmail.com>
2025-05-12 00:40:11 +0800

    Add DotNetGitPackageTest.sln

---
+++ DotNetGitPackageTest/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using DotNetPrivNupkgCiCdTest;
+
+namespace DotNetGitPackageTest
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        public MainViewModel()
+        {
+
+        }
+
+        [RelayCommand]
+        protected void RunTest()
+        {
+            var mh = new MathHelper();
+            var result = mh.Add(2, 3);
+        }
+    }
+}
